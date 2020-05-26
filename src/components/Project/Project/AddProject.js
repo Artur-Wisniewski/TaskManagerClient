b871b4afd372f0058d8e3a95fb07929b1d52a806
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-27 00:05:01 +0200

    Added Addproject view

--- src/components/Project/Project/AddProject.js
+++ src/components/Project/Project/AddProject.js
@@ -3,8 +3,41 @@ import React, {Component} from 'react';
 class AddProject extends Component {
     render() {
         return (
-            <div>
-                <h1>Add Project Form</h1>
+            <div className="project">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h5 className="display-4 text-center">Create / Edit Project form</h5>
+                            <hr/>
+                            <form>
+                                <div className="form-group">
+                                    <input type="text" className="form-control form-control-lg "
+                                           placeholder="Project Name"/>
+                                </div>
+                                <div className="form-group">
+                                    <input type="text" className="form-control form-control-lg"
+                                           placeholder="Unique Project ID"
+                                           />
+                                </div>
+
+                                <div className="form-group">
+                                    <textarea className="form-control form-control-lg"
+                                              placeholder="Project Description"></textarea>
+                                </div>
+                                <h6>Start Date</h6>
+                                <div className="form-group">
+                                    <input type="date" className="form-control form-control-lg" name="start_date"/>
+                                </div>
+                                <h6>Estimated End Date</h6>
+                                <div className="form-group">
+                                    <input type="date" className="form-control form-control-lg" name="end_date"/>
+                                </div>
+
+                                <input type="submit" className="btn btn-primary btn-block mt-4"/>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
