d0570d37dfe38cdfd41ae561495e88be7d1aaf0e
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-26 22:57:17 +0200

    Added Header

---
+++ src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div >
+                <h1 className="alert alert-warning"> Welcome to the Dashboard </h1>
+
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
