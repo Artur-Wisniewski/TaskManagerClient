aa5d5f15ab594a6d095ff21ed96657e77dce07b4
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-26 23:05:54 +0200

    Added Dashboard and ProjectItem

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
+import ProjectItem from "./Project/Project/ProjectItem";
 
 
 
 class Dashboard extends Component {
     render() {
         return (
-            <div >
-                <h1 className="alert alert-warning"> Welcome to the Dashboard </h1>
+            <div className="projects">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br/>
+                            <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                                Create a Project
+                            </a>
+                            <br/>
+                            <hr/>
 
+                            <ProjectItem></ProjectItem>
+
+
+
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
