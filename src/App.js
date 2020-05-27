7d414922217fb3ec0964adca22008bf7e9e430e7
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-27 23:01:18 +0200

    Added redux store

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import AddProject from "./components/Project/Project/AddProject";
-
+import {Provider} from "react-redux";
+import store from "./store";
 function App() {
   return (
-      <Router>
-          <div className="App">
-              <Header/>
-              <Route exact path="/dashboard" component={Dashboard}/>
-              <Route exact path="/addProject" component={AddProject}/>
-          </div>
-      </Router>
+      <Provider store={store}>
+          <Router>
+              <div className="App">
+                  <Header/>
+                  <Route exact path="/dashboard" component={Dashboard}/>
+                  <Route exact path="/addProject" component={AddProject}/>
+              </div>
+          </Router>
+      </Provider>
+
 
   );
 }
