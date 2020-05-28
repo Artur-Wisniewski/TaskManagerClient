ee1c8dcc7d7af5e760136b40f5be9c6ee5a5b8c8
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-28 21:12:40 +0200

    Refactored added proxy

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -4,7 +4,7 @@ import {GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT} from "./types";
 export const createProject = (project, history) => async dispatch =>{
     try{
         const res = await axios.post
-        ("http://localhost:8080/api/project", project);
+        ("/api/project", project);
         history.push("/dashboard");
 
         //cleaning errors when post passed
@@ -21,7 +21,7 @@ export const createProject = (project, history) => async dispatch =>{
 };
 
 export const getProjects = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080/api/project/all");
+  const res = await axios.get("/api/project/all");
   dispatch({
       type:GET_PROJECTS,
       payload: res.data
@@ -32,7 +32,7 @@ export const getProject = (id, history) => async dispatch => {
     //dispatch reducer with get_project
     //reducer gonna pass it to the state
     try{
-        const res = await axios.get(`http://localhost:8080/api/project/${id}` );
+        const res = await axios.get(`/api/project/${id}` );
         dispatch({
             type:GET_PROJECT,
             payload: res.data
@@ -45,7 +45,7 @@ export const getProject = (id, history) => async dispatch => {
 };
 export const deleteProject = id => async dispatch => {
 
-        const res = await axios.delete(`http://localhost:8080/api/project/${id}` );
+        const res = await axios.delete(`/api/project/${id}` );
         dispatch({
             type:DELETE_PROJECT,
             payload: id
