6d4b712bb7651de02a043172771c7047622e9866
Artur Wiśniewski <artwis1998@gmail.com>
2020-06-02 19:58:01 +0200

    Extracted backlog from ProjectBoard

--- src/components/Project/ProjectBoard/ProjectBoard.js
+++ src/components/Project/ProjectBoard/ProjectBoard.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
+import Backlog from "./Backlog";
 
 class ProjectBoard extends Component {
 
@@ -13,57 +14,8 @@ class ProjectBoard extends Component {
                 </Link>
                 <br/>
                 <hr/>
+                <Backlog/>
 
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-secondary text-white">
-                                    <h3>TO DO</h3>
-                                </div>
-                            </div>
-
-
-                            <div className="card mb-1 bg-light">
-
-                                <div className="card-header text-primary">
-                                    ID: projectSequence -- Priority: priorityString
-                                </div>
-                                <div className="card-body bg-light">
-                                    <h5 className="card-title">project_task.summary</h5>
-                                    <p className="card-text text-truncate ">
-                                        project_task.acceptanceCriteria
-                                    </p>
-                                    <a href="#" className="btn btn-primary">
-                                        View / Update
-                                    </a>
-
-                                    <button className="btn btn-danger ml-4">
-                                        Delete
-                                    </button>
-                                </div>
-                            </div>
-
-
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-primary text-white">
-                                    <h3>In Progress</h3>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-success text-white">
-                                    <h3>Done</h3>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
             </div>
         );
     }
