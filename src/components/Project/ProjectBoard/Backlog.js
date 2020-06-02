588c270b105597d1f67629005b1f8fb482657a04
Artur Wiśniewski <artwis1998@gmail.com>
2020-06-02 21:22:43 +0200

    Added task filter to backlog

--- src/components/Project/ProjectBoard/Backlog.js
+++ src/components/Project/ProjectBoard/Backlog.js
@@ -5,9 +5,17 @@ import ProjectTask from "./ProjectTask/ProjectTask";
 class Backlog extends Component {
 
     render() {
+
+        //TODO refactor this
         const {project_tasks} = this.props;
-        const tasks = project_tasks.map(project_task =>(
+        const todoTasks = project_tasks.filter(project_task => project_task.status === "TO_DO").map(project_task => (
+            <ProjectTask key={project_task.id} project_task={project_task}/>
+        ));
+       const inProgressTasks = project_tasks.filter(project_task => project_task.status === "IN_PROGRESS").map(project_task => (
            <ProjectTask key={project_task.id} project_task={project_task}/>
+       ));
+        const doneTasks = project_tasks.filter(project_task => project_task.status === "DONE").map(project_task => (
+            <ProjectTask key={project_task.id} project_task={project_task}/>
         ));
         return (
             <div className="container">
@@ -18,7 +26,7 @@ class Backlog extends Component {
                                 <h3>TO DO</h3>
                             </div>
                         </div>
-                        {tasks}
+                        {todoTasks}
 
 
 
@@ -30,6 +38,7 @@ class Backlog extends Component {
                                 <h3>In Progress</h3>
                             </div>
                         </div>
+                        {inProgressTasks}
 
                     </div>
                     <div className="col-md-4">
@@ -38,6 +47,7 @@ class Backlog extends Component {
                                 <h3>Done</h3>
                             </div>
                         </div>
+                        {doneTasks}
 
                     </div>
                 </div>
